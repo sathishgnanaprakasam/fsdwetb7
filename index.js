5e86472ff9ec0bc16e0f70d749ae32b4701d52de
githubscientist <sathish.datascientist@gmail.com>
2024-10-27 12:48:50 +0530

    classList

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-let advantages = [
-    `Dynamic content: DOM manipulation allows you to create dynamic, interactive web pages that respond to user input.`,
-    `Improved user experience: By updating the content of a page without reloading it, you can create a more seamless and engaging user experience.`,
-    `Accessibility: DOM manipulation can be used to make web pages more accessible to users with disabilities, by dynamically updating the content of the page based on user input.`
-]
+// let methodsParagraph = document.getElementsByClassName('methods')[0];
 
-let list = document.createElement('ul');
+// methodsParagraph.classList.add('greenText', 'bold');
 
-for (let i = 0; i < advantages.length; i++) {
-    let listItem = document.createElement('li');
+// methodsParagraph.classList.remove('greenText');
 
-    listItem.textContent = advantages[i];
+// methodsParagraph.classList.add('redText');
 
-    list.appendChild(listItem);
-}
+// methodsParagraph.classList.toggle('redText');
 
-let container = document.getElementsByClassName('container')[0];
+// console.log(methodsParagraph.classList.contains('italic'));
 
-let paragraph = document.createElement('p');
+// console.log(methodsParagraph.style);
 
-paragraph.textContent = 'Thus, DOM manipulation is a powerful tool for creating dynamic, interactive web pages that provide an improved user experience and increased accessibility.';
+// methodsParagraph.setAttribute('id', 'methods');
 
-container.appendChild(list);
-container.appendChild(paragraph);
-
-paragraph.remove();
-
-let header = document.getElementById('header');
-
-let newHeader = document.createElement('h1');
-
-newHeader.textContent = 'The Document Object Model (DOM)';
-
-// header.remove();
-
-header.replaceWith(newHeader);
-
-list.removeChild(list.childNodes[0]);
