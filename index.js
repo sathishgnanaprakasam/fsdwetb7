df801f0e112b050e618cbea3efddde1bf5deca7a
githubscientist <sathish.datascientist@gmail.com>
2024-11-02 10:49:54 +0530

    querySelector --revisited

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(document.querySelectorAll('.container>#header'));
+// let header = document.getElementById('header');
+// let header = document.querySelector('#header');
+
+// header.textContent = `A Blog on DOM Manipulation!`;
+
+// console.log(header.textContent);
+
+// const list = document.querySelectorAll('.greenText');
+
+// console.log(list);
+
+const paragraphs = document.querySelectorAll('p');
+
+paragraphs.forEach(p => {
+    p.textContent += '[This is a paragraph]';
+})
