6940af4830b838ee53842aff18a174074f611b33
githubscientist <sathish.datascientist@gmail.com>
2024-11-03 13:36:54 +0530

    wordle basic layout completed

--- index.js
+++ index.js
@@ -1,96 +1,38 @@
-let squares = document.querySelectorAll('.square');
-let status = document.querySelector('.status');
+let grid = document.querySelector('.grid');
 
-function createBoard() {
+let guesses = 6;
+let guessingRow = 0;
+let currentGuess = '';
 
-
-
-    // consider the squares to be 3 x 3 grid
-    squares.forEach((square, index) => {
-        // first row
-        if (index < 3) {
-            square.style.borderTop = 'none';
-        }
-
-        // last row
-        if (index > 5) {
-            square.style.borderBottom = 'none';
-        }
-
-        // first column
-        if (index % 3 === 0) {
-            square.style.borderLeft = 'none';
-        }
-
-        // last column
-        if (index % 3 === 2) {
-            square.style.borderRight = 'none';
-        }
-    });
+for (let i = 0; i < guesses * 5; i++) {
+    let div = document.createElement('div');
+    div.classList.add('cell');
+    grid.appendChild(div);
 }
 
-createBoard();
-
-let values = new Array(9).fill(null);
-let player = 'X';
-let winningLines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-
-];
-let winner = null;
-
-function checkWinner(values, player) {
-    for (let i = 0; i < winningLines.length; i++) {
-        let [a, b, c] = winningLines[i];
-
-        if (values[a] === player && values[b] === player && values[c] === player) {
-            return player;
-        }
+// general event listener
+document.addEventListener('keypress', (event) => {
+    if (currentGuess.length > 5) {
+        return;
     }
-    return null;
-}
 
-squares.forEach((square, index) => {
-    square.addEventListener('click', () => {
-        if (values[index] === null && !winner) {
-            square.textContent = player;
-            values[index] = player;
+    let key = event.key;
 
-            // check if the player has won
-            winner = checkWinner(values, player);
+    let isLetter = key.match(/[a-z]/i);
+    if (event.ctrlKey && key === 'd') {
+        grid.children[guessingRow * 5 + currentGuess.length - 1].textContent = '';
+        currentGuess = currentGuess.slice(0, -1);
+        console.log(currentGuess);
+    } else if (key === 'Enter') {
+        console.log('Enter key pressed');
+    } else if (isLetter && key.length === 1) {
+        grid.children[guessingRow * 5 + currentGuess.length].textContent = key;
+        currentGuess += key;
+        console.log(currentGuess);
 
-            if (winner) {
-                status.innerHTML = `Winner: ${winner}`;
-                return;
-            }
-
-            player = player === 'X' ? 'O' : 'X';
-
-            status.innerHTML = `Next player: ${player}`;
-
-            if (values.indexOf(null) === -1) {
-                status.innerHTML = 'Status: Game Draw';
-            }
-        }
-    });
-});
-
-let restart = document.querySelector('.restart');
-
-restart.addEventListener('click', () => {
-    squares.forEach(square => {
-        square.textContent = '';
-    });
-
-    values = new Array(9).fill(null);
-    player = 'X';
-    winner = null;
-    status.innerHTML = `Next player: ${player}`;
-});
+        // only after 5 letters are entered followed by enter key
+        // if (grid.children[guessingRow * 5 + 4].textContent) {
+        //     guessingRow++;
+        // }
+    }
+})
