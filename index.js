26cc34c60d69a26dcf3608e9554ed62a10979122
githubscientist <sathish.datascientist@gmail.com>
2024-11-10 13:31:12 +0530

    promises --example using file copying

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-let header = document.querySelector('h1');
+const readline = require('readline');
+const fs = require('fs');
 
-// header.style.color = 'red';
-// header.setAttribute('style', 'color: red;');
-// header.classList.add('redColor');
-header.setAttribute('class', 'redColor');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(header);
+function copyFile(source, destination) {
+    try {
+        fs.copyFileSync(source, destination);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+const promise = new Promise((resolve, reject) => {
+    rl.question('Enter the destination file name: ', (filename) => {
+        if (copyFile('source.txt', filename)) {
+            resolve();
+        } else {
+            reject();
+        }
+
+        rl.close();
+    });
+});
+
+promise
+    .then(() => {
+        console.log('File copied successfully');
+    })
+    .catch(() => {
+        console.log('Error copying file');
+    });
