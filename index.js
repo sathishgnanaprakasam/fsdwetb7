f97e2e297a7ab627e4390ab7ce6920c1ee832105
githubscientist <sathish.datascientist@gmail.com>
2024-11-10 13:14:42 +0530

    css handling

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-let url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/';
+let header = document.querySelector('h1');
 
-async function fetchMeaning(word) {
-    // make an api call to fetch the meaning of the word
-    // using the fetch function
-    let response;
-    let data;
-    try {
-        response = await fetch(url + word);
+// header.style.color = 'red';
+// header.setAttribute('style', 'color: red;');
+// header.classList.add('redColor');
+header.setAttribute('class', 'redColor');
 
-        if (response.ok) {
-            try {
-                data = await response.json();
-            } catch (error) {
-                console.log('Error in parsing JSON: ', error);
-            }
-        }
-    } catch (error) {
-        console.log('Error in fetching data: ', error);
-    }
-
-    console.log(data);
-}
-
-let word = 'apple';
-fetchMeaning(word);
+console.log(header);
