1a7ce2cb70f0a710e41c26ba6eecb8cb4e432937
githubscientist <sathish.datascientist@gmail.com>
2024-11-23 13:09:48 +0530

    day 21: ES5 Vs ES6

--- index.js
+++ index.js
@@ -1,83 +1,247 @@
-async function loadTodos() {
-    // Get the reference of the todoList div
-    const todoList = document.querySelector('.todoList');
+/*
+    ES5 Vs ES6
 
-    // get the todos from the localStorage
-    const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+    1. var vs let/const
+    2. arrow function
+    3. template literals
+    4. default parameters
+    5. destructuring
+    6. rest and spread operator
+    7. class
+    8. import/export
+    9. promise
+    10. async/await
+    11. fetch
+    12. for...of
+    13. for...in
+*/
 
-    todoList.innerHTML = '';
+// var vs let/const
+// let a = 5; // a global variable
 
-    // Create an unordered list
-    const ulist = document.createElement('ul');
+// {
+//     let a = 10;
 
-    // loop through the data
-    // for every todo item in the data
-    todos.forEach(todo => {
-        // create an anchor tag
-        const anchor = document.createElement('a');
+//     console.log(`Inside block: ${a}`);
+// }
 
-        // set the href attribute of the anchor tag
-        anchor.href = `./src/todo.html?id=${todo.id}`;
+// console.log(`Outside block: ${a}`);
 
-        // create a html list item
-        const listItem = document.createElement('li');
+// let a = 5;
 
-        // add the content - title of the todo item to the html list item
-        listItem.textContent = todo.title;
+// a = 10; // variable reassignment is allowed
 
-        anchor.appendChild(listItem);
+// console.log(a);
 
-        // append the list item to the unordered list
-        ulist.appendChild(anchor);
-    });
+// const a = 5;
 
-    // after exiting the loop, append the unordered list to the html todoList div
-    todoList.appendChild(ulist);
+// a = 10; // variable reassignment is not allowed
+
+// console.log(a);
+
+// let x = 10;
+
+// if (true) {
+//     let x = 20;
+
+//     console.log(`Inside block: ${x}`);
+// }
+
+// console.log(`Outside block: ${x}`);
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+// var a = 10; // a global variable
+
+// function test() {
+//     var a = 5;
+//     a = 20;
+
+//     console.log(`Inside function: ${a}`);
+// }
+
+// test();
+// console.log(`Outside function: ${a}`);
+
+// named functions
+// function sayHello() {
+//     console.log('Hello World!');
+// }
+
+// sayHello();
+
+// nameless functions or anonymous functions
+// let sayHello = function () {
+//     console.log('Hello World!');
+// }
+
+// sayHello();
+
+// arrow function
+// let sayHello = () => {
+//     console.log('Hello World!');
+// }
+
+// sayHello();
+
+// Immediately Invoked Function Expression
+// (() => {
+//     console.log('Hello World!');
+// })();
+
+// (function () {
+//     console.log('Hello World!');
+// })();
+
+/*
+    Template Literals
+
+    1. We can use multi-line strings.
+    2. We can use string interpolation: ${expression}
+
+*/
+
+// let paragraph = `Apple is a fruit and
+// it is red in color.`;
+
+// console.log(paragraph);
+
+// let name = 'John';
+// let age = 30;
+
+// // console.log('My name is ' + name + ' and I am ' + age + ' years old.');
+
+// console.log(`My name is ${name} and I am ${age} years old.`);
+
+// let person = {
+//     name: 'John',
+//     age: 30
+// }
+
+// let person = ['John', 30];
+
+// console.log(`${person}`);
+
+// default parameters
+// function sayHello(name = 'guest') {
+//     console.log(`Hello ${name}!`);
+// }
+
+// sayHello('krish');
+
+//  destructuring: destructuring is a way to extract values from objects and arrays.
+// let person = ['John', 30];
+
+// let name = person[0];
+// let age = person[1];
+
+// let [name, age] = ['John', 30];
+
+// let person = ['John', 30];
+
+// let [name, age] = person;
+
+// console.log(`Name: ${name}`);
+// console.log(`Age: ${age}`);
+
+// let person = {
+//     name: 'John',
+//     age: 30
+// }
+
+// // let name = person.name;
+// // let age = person.age;
+// let { age, name: label } = person;
+
+// console.log(`Name: ${label}`);
+// console.log(`Age: ${age}`);
+
+// rest and spread operator: ... (three dots)
+
+// let nums1 = [1, 2, 3];
+// let nums2 = [4, 5, 6];
+
+// // console.log(...nums1);
+// // console.log(1, 2, 3);
+
+// let nums = [...nums1, ...nums2];
+
+// console.log(nums);
+
+// function sum(...nums) {
+//     console.log(nums.reduce((p, c) => p + c));
+// }
+
+// sum(5, 6, 7);
+// function reducer(previousValue, currentValue) {
+//     return previousValue + currentValue;
+// }
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.reduce(reducer));
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.reduce((p, c) => p + c));
+
+/*
+    previousValue = 1
+    currentValue = 2
+
+    previousValue = 3
+    currentValue = 3
+
+    previousValue = 6
+    currentValue = 4
+
+    previousValue = 10
+    currentValue = 5
+
+    15
+*/
+
+// let [a, , , b, c] = [4, 5, 6, 7, 8];
+
+// console.log(a, b, c);
+
+// let numbers = new Array(1, 2, 3);
+
+// console.log(numbers);
+
+/*
+
+    dynamic typing: the type of a variable is determined at runtime.
+
+*/
+
+// function prototyping
+// function DOB(date, month, year) {
+//     this.date = date;
+//     this.month = month;
+//     this.year = year;
+// }
+
+// let dob1 = new DOB(23, 'November', 2024);
+// let dob2 = new DOB(22, 'December', 2025);
+
+// console.log(dob1);
+// console.log(dob2);
+
+class DOB {
+    constructor(date, month, year) {
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
 }
 
-loadTodos();
+let dob1 = new DOB(23, 'November', 2024);
+let dob2 = new DOB(22, 'December', 2025);
 
-// get the reference of the addForm
-const addForm = document.querySelector('.addForm');
-
-addForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    // get the title and description from the form using the form elements
-    const title = addForm.todoTitle.value;
-    const description = addForm.todoDescription.value;
-    const isCompleted = addForm.todoCompleted.checked;
-
-    const todo = {
-        title,
-        description,
-        isCompleted
-    };
-
-    try {
-        // get the todos from the localStorage
-        let lstodos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
-
-        // get the id of the last todo item
-        const lastTodo = lstodos[lstodos.length - 1];
-
-        todo.id = lastTodo ? lastTodo.id + 1 : 1;
-
-        // add the new todo to the todos array
-        lstodos.push(todo);
-
-        // set the todos to the localStorage
-        localStorage.setItem('todos', JSON.stringify(lstodos));
-
-        // clear the form
-        addForm.reset();
-
-        alert('Todo added successfully');
-
-        // redirect the user to the home page
-        // window.location.href = '/index.html';
-        loadTodos();
-    } catch (error) {
-        alert('Failed to add todo');
-    }
-});
+console.log(dob1);
+console.log(dob2);
