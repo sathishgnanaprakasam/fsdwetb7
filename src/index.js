6fc83f751c8469d1c0a43cacf388844e0e91941b
githubscientist <sathish.datascientist@gmail.com>
2024-12-01 10:32:23 +0530

    empty repo with only readme.md

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// React component
-const App = () => {
-    return <h1>Hello, Students!!</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
