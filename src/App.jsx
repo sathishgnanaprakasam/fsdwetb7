02348f7d1ed247b71b1b8139f4e323693340ca5e
githubscientist <sathish.datascientist@gmail.com>
2024-12-08 12:57:03 +0530

    api call in react component

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,28 @@ import { useEffect, useState } from "react";
 
 const App = () => {
 
-    const [like, setLike] = useState(0);
-    const [dislike, setDislike] = useState(0);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        console.log('This will run only once when the component renders for the first time');
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
+            .then((response) => {
+                return response.json();
+            })
+            .then(data => {
+                setPosts(data);
+            })
+            .catch((error) => {
+                console.log('Error fetching posts:', error);
+            })
     }, []);
 
     useEffect(() => {
-        console.log('This will run when the component renders and for any state changes and all the re-renders');
-    });
-
-    useEffect(() => {
-        console.log('This will run when the component renders and on every state change of "like" state');
-    }, [like]);
+        console.log(posts);
+    }, [posts]);
 
     return (
         <div>
-            <button onClick={() => setLike(like + 1)}>
-                <span className="material-symbols-outlined">
-                    thumb_up
-                </span> {like}
-            </button>
-            <button onClick={() => setDislike(dislike + 1)}>
-                <span className="material-symbols-outlined">
-                    thumb_down
-                </span> {dislike}
-            </button>
+            <h1>Posts</h1>
         </div>
     )
 }
