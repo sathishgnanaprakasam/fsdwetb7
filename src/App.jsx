aee60d95361f9ebe8cf8fec3b0fea5639d45511c
githubscientist <sathish.datascientist@gmail.com>
2024-12-14 12:50:07 +0530

    useRef hook --value persists across renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,20 @@
-import { useReducer } from "react";
-
-const intialState = {
-    count: 0,
-    history: []
-};
-
-const reducer = (state, action) => {
-
-    switch (action.type) {
-        case 'PLUS':
-            return {
-                count: state.count + 1,
-                history: [...state.history, 'PLUS']
-            }
-        case 'MINUS':
-            return {
-                count: state.count - 1,
-                history: [...state.history, 'MINUS']
-            }
-        case 'RESET':
-            return {
-                count: 0,
-                history: [...state.history, 'RESET']
-            }
-        default:
-            return state;
-    }
-}
+import { useRef, useState } from "react";
 
 const App = () => {
 
-    const [state, dispatch] = useReducer(reducer, intialState);
+    const count = useRef(0);
+    const [updated, setUpdated] = useState(0);
+
+    const handleIncrement = () => {
+        count.current++;
+        console.log(count.current);
+        setUpdated((prev) => prev + 1);
+    }
 
     return (
         <div>
-            <h1>Count: {state.count}</h1>
-            <button onClick={() => dispatch({ type: "PLUS" })}>Increase</button>
-            <button onClick={() => dispatch({ type: "MINUS" })}>Decrease</button>
-            <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
-
-            <h2>History</h2>
-            {
-                state.history.join(', ')
-            }
+            <h1>Count: {count.current}</h1>
+            <button onClick={handleIncrement}>Increment</button>
         </div>
     )
 }
