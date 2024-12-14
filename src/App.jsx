539404bba9149be2b0a970f87b9b78bc18cd9c12
githubscientist <sathish.datascientist@gmail.com>
2024-12-14 11:12:47 +0530

    state management --useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const App = () => {
 
-    let count = 0;
-    const [updated, setUpdated] = useState(0);
+    const [count, setCount] = useState(0);
 
     const handleIncrease = () => {
-        count++;
-        setUpdated(updated + 1);
+        setCount(count + 1);
     }
-
-    useEffect(() => {
-        console.log(count);
-    }, [updated]);
 
     return (
         <div>
-            <h1>Count: {updated}</h1>
+            <h1>Count: {count}</h1>
             <button onClick={handleIncrease}>Increase</button>
         </div>
     )
