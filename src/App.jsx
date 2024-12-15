bf8a5d1d32e9d720d6dc37175d426ac02d75e552
githubscientist <sathish.datascientist@gmail.com>
2024-12-15 13:11:44 +0530

    shopping cart --task doubt

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
 const App = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products`, {
+            method: 'GET'
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setProducts(data);
+            })
+    }, []);
+
+    useEffect(() => {
+        console.log(products);
+    }, [products]);
+
     return (
         <div>
-            <h1 className="text-3xl font-bold text-green-700 text-center my-4">Hello World!</h1>
+            <h1>Products</h1>
+            <div className="products">
+                {
+                    products.map(product => (
+                        <div key={product.id}>
+                            <h2 onClick={() => console.log(product.title, product.id)}>{product.title}</h2>
+                            <p>{product.description}</p>
+                            <p>{product.price}</p>
+                            <img src={product.image} alt={product.title}
+                                style={{ width: '100px', height: '100px' }}
+                            />
+                            <hr />
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
