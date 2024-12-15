77987c2d738a8ffb1c0f9f78760b82559d97cb0b
githubscientist <sathish.datascientist@gmail.com>
2024-12-15 10:49:57 +0530

    useCallback --example 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { useCallback, useState } from "react"
 
 const App = () => {
 
-    const [count, setCount] = useState(0);
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
 
-    const handleIncrement = useCallback(() => {
-        setCount(count + 1);
-    }, [count]);
+    const handleLike = useCallback(() => {
+        setLikes(likes + 1);
+    }, [likes]);
+
+    const handleDislike = useCallback(() => {
+        setDislikes(dislikes + 1);
+    }, [dislikes]);
+
 
     return (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={handleIncrement}>Increment</button>
+            <h1>likes: {likes}</h1>
+            <button onClick={handleLike}>Like</button>
+            <h1>dislikes: {dislikes}</h1>
+            <button onClick={handleDislike}>Dislike</button>
         </div>
     )
 }
