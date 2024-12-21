01079c36c4e9c22d3de6c64d83914ffcc92a030e
githubscientist <sathish.datascientist@gmail.com>
2024-12-21 12:49:26 +0530

    data fetching --useEffect

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Users = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(response => response.json())
+            .then(data => setUsers(data));
+    }, []);
+
+    console.log(users);
+
     return (
         <div>
             <h1>Users</h1>
-            <p>Users page content</p>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
